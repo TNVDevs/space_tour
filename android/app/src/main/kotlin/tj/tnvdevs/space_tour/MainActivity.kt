b0abedd12070efff139707fb65a7bb096d8b44e8
Faridun Fatkhullozoda <faridunf200630@gmail.com>
2025-10-03 20:52:18 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/tj/tnvdevs/space_tour/MainActivity.kt
@@ -0,0 +1,5 @@
+package tj.tnvdevs.space_tour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainAppp());
+}
+
+class MainAppp extends StatelessWidget {
+  const MainAppp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
